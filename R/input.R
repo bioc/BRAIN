bacff66021a9a0f809d860685778617e307e9f79
Piotr Dittwald <piotr.dittwald@mimuw.edu.pl>
2013-07-26 11:44:18 +0000

    small corections in 17O mass plus references in inpur.R file
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_12/madman/Rpacks/BRAIN@78875 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/input.R
+++ R/input.R
@@ -17,7 +17,7 @@ listMass <- list(
   C = c(12.0000000000, 13.0033548378),
   H = c(1.0078250321, 2.0141017780),
   N = c(14.00307400522, 15.0001088984),
-  O = c(15.9949146, 16.999132, 17.9991603),
+  O = c(15.9949146, 16.9991312, 17.9991603),
   S = c(31.97207070, 32.97145843, 33.96786665, 0, 35.96708062),
   F = c(18.9984),
   Br = c(78.9183371, 0, 80.9162906),
@@ -28,7 +28,17 @@ listMass <- list(
   K = c(38.9637074, 39.9639992, 40.9618254)
 )
 
-#compare with: http://www.chem.ualberta.ca/~massspec/atomic_mass_abund.pdf
+##IUPAC1997 for C, H, N, O, S
+
+##see also 
+## 'Reply to the Comment on:
+# “An Efficient Method to Calculate the Aggregated Isotopic Distribution and Exact Center-Masses” by Jürgen Claesen, # Piotr Dittwald, Tomasz Burzykowski, Dirk Valkenborg, J. Am. Soc. Mass Spectrom . 23, 753–763 (2012)'
+# Jürgen Claesen  et al., Journal of The American Society for Mass Spectrometry,
+# October 2012, Volume 23, Issue 10, pp 1828-1829 
+
+
+## 
+#compare also with: http://www.chem.ualberta.ca/~massspec/atomic_mass_abund.pdf
 
 # in header of this document:
 
@@ -37,36 +47,3 @@ listMass <- list(
 # Abundance Measurements by K.J.R. Rosman, P.D.P. Taylor Pure Appl. Chem. 1999, 71, 1593-1607."
 
 
-
-#listIso <- list(
-#  C = c(98.93, 1.07)/100,
-#  H = c(99.9885, 0.0115)/100,
-#  N = c(99.632, 0.368)/100,
-#  O = c(99.757, 0.038, 0.205)/100,
-#  S = c(94.93, 0.76, 4.29, 0, 0.02)/100
-#)
-
-#listMass <- list( #IUPAC
-#  C = c(12.0000000000, 13.0033548378),
-#  H = c(1.0078250321, 2.0141017780),
-#  N = c(14.00307400522, 15.0001088984),
-#  O = c(15.9949146, 16.9991312, 17.9991603),
-#  S = c(31.97207070, 32.97145843, 33.96786665, 0, 35.96708062)
-#)
-
-# listMass <- list( #test
-#   C = c(12.0000000000, 13.0034),
-#   H = c(1.007, 2.013),
-#   N = c(14.00307400522, 15.0001088984),
-#   O = c(15.9949146, 16.9991312, 17.9991604),
-#   S = c(31.97207069, 32.97145850, 33.96786683, 0, 35.96708088)
-# )
-# 
-# 
-# listIso <- list(#test
-#   C = c(98.93111, 1.07899)/100,
-#   H = c(99.9885, 0.0115)/100,
-#   N = c(99.632, 0.368)/100,
-#   O = c(99.757, 0.038, 0.205)/100,
-#   S = c(94.93, 0.76, 4.29, 0, 0.02)/100
-# )
